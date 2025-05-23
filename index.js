7da9f3a75d106c65843c6e54b367243c765cd8d4
ianrking <107558226+ianrking@users.noreply.github.com>
2025-05-23 10:20:42 -0400

    Create greeting in index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function greet() {
+    let name = prompt("What is your name?");
+    
+    if (name !== null) {
+        alert("Hello, " + name + ".");
+        let age = prompt("How old are you?");
+        if (age !== null) {
+            console.log(parseInt(age));
+            if (confirm("Have you had a birthday this year?")) {
+            const birthYear = new Date().getFullYear() - age;
+            alert("You were born in the year " + birthYear + ".");
+            } else {
+                const birthYear = new Date().getFullYear() - age + 1;
+                alert("You were born in the year " + birthYear + ".");
+            }
+        }
+    }
+}
+
+greet()
